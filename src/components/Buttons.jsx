fc984cc0ae3b1e11b474c65da65f52802510bc93
Hengborann Moul <moulhengborann@gmail.com>
2021-01-28 18:06:22 +0700

    First Commit

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { GitHub, LinkedIn, Resume, Email } from './../icons'
+
+const DATA = [
+    {
+        href: "https://github.com/Hengborann-Moul",
+        aria: "Visit my GitHub profile to learn more about the projects I have created and contributed to",
+        icon: <GitHub />,
+        label: "Github",
+    },
+    {
+        href: "https://www.linkedin.com/in/moul-hengborann-35a307115/",
+        aria: "Visit my LinkedIn profile to learn more about my education and work experience",
+        icon: <LinkedIn />,
+        label: "LinkedIn",
+    },
+    {
+        href: "https://drive.google.com/drive/folders/10k8NWflSYQ5laPzuWtK3bzUKzuOeas8i?usp=sharing",
+        aria: "Visit Google Drive to view and download a copy of my resume",
+        icon: <Resume />,
+        label: "Resume",
+    },
+    {
+        href: "mailto:moulhengborann@gmail.com",
+        aria: "Open a pre-addressed email prompt to me that you can fill out",
+        icon: <Email />,
+        label: "Email",
+    }
+];
+
+const Button = ({ href, aria, icon, label }) => {
+    return (
+        <span className='button-container'>
+            <a className='button' href={href} target='_blank' aria-label={aria} rel='noopener noreferrer'>
+                <div className='icon'>{icon}</div>
+                <span className='icon_title'>{label}</span>
+            </a>
+        </span>
+    );
+};
+
+class Buttons extends Component {
+    render() {
+        return (
+            <div>
+                {DATA.map((props, i) => (
+                    <Button {...props} key={i} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Buttons;
