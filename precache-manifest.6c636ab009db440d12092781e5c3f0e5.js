81975a837f5ad71fe817882dd1b57405cd43d2ee
Hengborann-Moul <moulhengborann@gmail.com>
2021-07-12 22:44:08 +0700

    Updates

---
+++ precache-manifest.6c636ab009db440d12092781e5c3f0e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feb4c0d04585b8f2d42396752844ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc5a6de914b9a2970be3",
+    "url": "/static/css/main.738e56b2.chunk.css"
+  },
+  {
+    "revision": "475ed3912329456d0087",
+    "url": "/static/js/2.3c03fef7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c03fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5a6de914b9a2970be3",
+    "url": "/static/js/main.150a2536.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d148f82972402f9d457ae853e8d326b6",
+    "url": "/static/media/profile-min.d148f829.jpg"
+  }
+]);
