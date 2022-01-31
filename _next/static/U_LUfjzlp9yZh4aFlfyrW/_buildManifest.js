8828092e0288f24c098c160a726a4bb0d1f02d6c
Hengborann-Moul <Hengborann-Moul@users.noreply.github.com>
2022-01-31 14:53:51 +0000

    Deploying to gh-pages from  @ 3de99a3c529b80ea5ef99469c64bec0e575c7428 🚀

---
+++ _next/static/U_LUfjzlp9yZh4aFlfyrW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-20da3b41919cd005.js","static/css/03631bb6d5445694.css","static/chunks/pages/index-bb10717eff40fe35.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
