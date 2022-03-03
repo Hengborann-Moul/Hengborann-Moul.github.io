3d3baeed3880cff23f691cb37fe5f55bd1acfaa9
hengborann.moul <moulhengborann@gmail.com>
2022-03-03 17:33:58 +0700

    feat(tailwind): move from CSS to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      color: '#adbac7',
+      colorPrimary: '#0d1117',
+      colorSecondary: '#fdf9f3',
+      bg: '#22272e',
+      bgSecondary: '#22272e',
+      bgCode: '#ddd',
+      borderColor: '#adbac7',
+    }
+  },
+  plugins: [],
+};
