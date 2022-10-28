0ff05da5b894427f3ad83ae9ab4dcd901938fe7f
hengborann.moul <moulhengborann@gmail.com>
2022-10-28 15:38:02 +0700

    feat: blogs are coming

---
+++ pages/blogs/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+export default function Blogs() {
+  return (
+    <div>
+      <h1 className={styles.title}>Coming soon!</h1>
+    </div>
+  );
+}
