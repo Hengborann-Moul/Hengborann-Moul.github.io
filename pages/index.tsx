a1292a3121de07c803e408bc7be0c83f6eb57566
hengborann.moul <moulhengborann@gmail.com>
2022-11-19 16:47:15 +0700

    feat: remove major scss file

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,9 @@ import { ActionsList } from "../components/ActionsList/actions-list.component";
 import { Content } from "../components/Content/content.component";
 import { Particle } from "../components/Particle/particle.component";
 
-import styles from "../styles/Home.module.scss";
-
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Content />
       <ActionsList />
       <Particle />
